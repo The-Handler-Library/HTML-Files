0ea876f47c6d73a8036c2a13410a7163ef88e0be
mdrumsta <71355915+mdrumsta@users.noreply.github.com>
2020-11-29 20:19:47 -0500

    Add files via upload
    Updated header file with css applications.

--- header.php
+++ header.php
@@ -3,12 +3,20 @@
 ?>
 
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
+    <link rel="icon" href="/images/tcl_aqua_logo.jpg">
+
     <meta charset="UTF-8">
+    
     <meta name=viewport content = "width=device-width, initial-scale=1">
+    
     <title> Home Page </title>
-    <link href="/style.css" rel="stylesheet" type="text/css">
+    
+    <link href="style.css" rel="stylesheet" type="text/css">
+  
+
 
 </head>
 
@@ -18,27 +26,27 @@
         
         <nav class= "navbar">
             
-            <img class="logo" src="images/logo2.jpeg">
+            <img class="logo" src="logo2.jpeg">
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
                 <li><a href="contact.html">Contact</a></li>
-                <li><a href="library_db.php">Catalogue</a></li>
-                <li><a href="store.html">Store</a></li>
-                
+                <li><a href="library_db.php">Catalog</a></li>
                 <?php
                 if(isset($_SESSION["useruid"])){
-                    echo "<li><a href='profile.php'>Profile Page</a></li>";
                     echo "<li><a href='friends.php'>Friends</a></li>";
                     echo "<li><a href='logout.inc.php'>Log out</a></li>";
+                   
                 }
                 else{
                     echo "<li><a href='signup.php'>Signup</a></li>";
                     echo "<li><a href='login.php'>LOGIN</a></li>";
+                    
+                    
                     }
                 ?>
-                
+               
             </ul>
         </nav>
-        <div class="center">
+  
