a4354a275fa5dc0046f31e57df9188c3ae872899
mdrumsta <71355915+mdrumsta@users.noreply.github.com>
2020-11-29 20:20:15 -0500

    Add files via upload
    Updated index file with css applications.

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
     include_once 'header.php';
 ?>
-            <h1>Capstone Library</h1>
+ <link href="style.css" rel="stylesheet" type="text/css">
+            <h1>Handler Library</h1>
+            <h3>Read Books and Make Friends</h3>
             
-            <div class="container">
+            <div class="buttons">
                     <?php
                 if(isset($_SESSION["useruid"])){
                     echo "<p>You are signed in " . $_SESSION["useruid"]. "</p>" ;
@@ -11,18 +13,32 @@
                 }
                 
                 ?>
-                 <?php
+                <div class="btn1">
+                  <?php
                 if(isset($_SESSION["useruid"])){
-                    echo "<li><a href='search.php'>Search for books</a></li>";
-                    echo "<li><a href='message.php'>Chat with friends</a></li>";
+                    echo "<li><a href='store.html'>Search for books</a></li>";
+                  
                 }
                 else{
                     echo "<li><a href='signup.php'>Signup to create an account</a></li>";
-                    echo "<li><a href='login.php'>Login to search our inventory</a></li>";
-                    
                     
                     }
                 ?>
+                </div>
+                
+                <div class="btn2">
+                  <?php
+                if(isset($_SESSION["useruid"])){
+                 echo "<li><a href='message.php'>Chat with friends</a></li>";
+
+                }
+                else{
+                    echo "<li><a href='login.php'>Login to search our inventory</a></li>";
+                    
+                    }
+                ?>
+                </div>
+                
             </div>
  <?php
     include_once 'footer.php';
